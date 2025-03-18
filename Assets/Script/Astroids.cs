2a568242022971e55eebff62d83ea268b913549c
DocBrown19 <bluejb1985@gmail.com>
2025-03-18 14:57:58 -0500

    Planets

--- Assets/Script/Astroids.cs
+++ Assets/Script/Astroids.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
+using System.Collections;
 
 public class Asteroids : MonoBehaviour
 {
-    private Vector3 randomSpin;
-    private float speed;
+    // private Vector3 randomSpin;
+    // private float speed;
+    public int checkPlayerDistanceTime = 5;
+    public Transform playerTransform;
+    public float maxDistenceFromPlayer = 1000f;
     void Start()
     {
-        randomSpin = new Vector3(Random.Range(-359f, 359f), Random.Range(-359f, 359f), Random.Range(-359f, 359f));
-        speed = Random.Range(0, .25f);
+        // randomSpin = new Vector3(Random.Range(-359f, 359f), Random.Range(-359f, 359f), Random.Range(-359f, 359f));
+        // speed = Random.Range(0, .25f);
+        playerTransform = GameObject.Find("Player Ship").transform;
+        StartCoroutine(CheckPlayerDistance());
+    }
+    IEnumerator CheckPlayerDistance()
+    {
+      yield return new WaitForSeconds(checkPlayerDistanceTime);
+        if (Vector3.Distance(playerTransform.position, transform.position) > maxDistenceFromPlayer)
+        {
+           Destroy(gameObject); 
+        }
+        StartCoroutine(CheckPlayerDistance());
     }
 
-    
+
     void Update()
     {
-      transform.Rotate(randomSpin* Time.deltaTime*speed);  
+     // transform.Rotate(randomSpin* Time.deltaTime*speed);  
     }
 }
